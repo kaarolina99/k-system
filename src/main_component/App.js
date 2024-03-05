105747707ef6639c51db0d6a6b8ccc0aef1f6668
kaarolina99 <kapaa008@osloskolen.no>
2024-03-05 15:23:18 +0100

    eksempel navbar

--- src/main_component/App.js
+++ src/main_component/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import {QueueGrid} from '../queue_component/App';
-import {FormsGrid} from '../forms_component/App';
-
-
+import React from "react";
+import "./App.css";
+import { QueueGrid } from "../queue_component/App";
+import { FormsGrid } from "../forms_component/App";
+import Navbar from "../navbar_component/App";
 
 export default function MyApp() {
   return (
-    <div> 
-      <QueueGrid/>
-      <FormsGrid/>
+    <div>
+      <Navbar />
+      <QueueGrid />
+      <FormsGrid />
     </div>
   );
 }
-
-
--- src/navbar_component/App.js
+++ src/navbar_component/App.js
@@ -1,1 +1,22 @@
-import './App.css';
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
