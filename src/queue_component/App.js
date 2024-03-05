d6925b57b7cfa10e32c14bef2b932079cef722d0
kaarolina99 <kapaa008@osloskolen.no>
2024-03-05 11:06:14 +0100

    array

--- src/queue_component/App.js
+++ src/queue_component/App.js
@@ -1,15 +1,36 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let test_queue = [
+  { name: "Karolina", sted: "Amfiet" },
+  { name: "Julian", sted: "Utsikten" },
+  { name: "Sara", sted: "Blåbås" },
+  { name: "Henrik", sted: "Langbord" },
+  { name: "Erik", sted: "Grønnbås" },
+  { name: "Tidemann", sted: "Amfiet" },
+];
 
 export function QueueGrid() {
-return (
+  return (
     <div className="queueGridContainer">
-        <div className="QueueGrid">
-            <div>
-                <div className="ActiveQueue">
-                    <h1>Kø</h1>
+      <div className="QueueGrid">
+        <div>
+          <div className="KContainer">
+            <h1>Kø</h1>
+          </div>
+          <div className="ActiveQueue">
+            <div className="queueItems">
+              {test_queue.map((person, index) => (
+                <div key={index} className="queueItem">
+                  <p>
+                    Name: {person.name} Sted: {person.sted}
+                  </p>
                 </div>
+              ))}
             </div>
+          </div>
         </div>
+      </div>
     </div>
-);
+  );
 }
