be88d60c219a9dfa6e343de8a42cabc1d6c1aaf4
kaarolina99 <kapaa008@osloskolen.no>
2024-04-05 10:27:18 +0200

    Fixed import error

--- src/forms_component/App.js
+++ src/forms_component/App.js
@@ -1,36 +1,49 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function NextButton(){
-    return(
-        <form action="">
-            <input type="Submit" value="Next"/>
-        </form>
-    );
+function NextButton() {
+  return (
+    <form action="">
+      <input type="Submit" value="Next" />
+    </form>
+  );
 }
 
 function handleKeyDown(event) {
-    if (event.keyCode === 13) {
-    console.log('Enter key was pressed');
-    }
+  if (event.keyCode === 13) {
+    console.log("Enter key was pressed");
+  }
 }
 
-
-
 export function FormsGrid() {
-return (
+  return (
     <div className="formsGridContainer">
-        <div className="FormsGrid">
-            <div className="InputForms">
-                <h1> Skriv deg opp i køen </h1>
-                <form>
-                    <label htmlFor="Navn"> Navn </label><br />
-                    <input type="text" id="Navn_" name="Navn" onKeyDown={handleKeyDown}/><br />
-                    <label htmlFor="Sted"> Sted </label><br />
-                    <input type="text" id="Sted_" name="Sted" onKeyDown={handleKeyDown}/>
-                </form>
-                <br /><NextButton/>
-            </div>
+      <div className="FormsGrid">
+        <div className="InputForms">
+          <h1> Skriv deg opp i køen </h1>
+          <form>
+            <label htmlFor="Navn"> Navn </label>
+            <br />
+            <input
+              type="text"
+              id="Navn_"
+              name="Navn"
+              onKeyDown={handleKeyDown}
+            />
+            <br />
+            <label htmlFor="Sted"> Sted </label>
+            <br />
+            <input
+              type="text"
+              id="Sted_"
+              name="Sted"
+              onKeyDown={handleKeyDown}
+            />
+          </form>
+          <br />
+          <NextButton />
         </div>
+      </div>
     </div>
-);
+  );
 }
