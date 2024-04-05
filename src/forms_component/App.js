b51177ec6a57fdc28d139059957680aa6923f491
kaarolina99 <kapaa008@osloskolen.no>
2024-04-05 10:22:38 +0200

    alt

--- src/forms_component/App.js
+++ src/forms_component/App.js
@@ -1,36 +1,53 @@
-import './App.css';
-
-function NextButton(){
-    return(
-        <form action="">
-            <input type="Submit" value="Next"/>
-        </form>
-    );
-}
+import React from "react";
+import "./App.css";
 
 function handleKeyDown(event) {
-    if (event.keyCode === 13) {
-    console.log('Enter key was pressed');
-    }
+  if (event.keyCode === 13) {
+    console.log("Enter key was pressed");
+  }
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
 
+  const form = event.target;
+  const formData = new FormData(form);
+
+  const formJson = Object.fromEntries(formData.entries());
+  console.log("YOLO", formJson);
+}
 
 export function FormsGrid() {
-return (
+  return (
     <div className="formsGridContainer">
-        <div className="FormsGrid">
-            <div className="InputForms">
-                <h1> Skriv deg opp i køen </h1>
-                <form>
-                    <label htmlFor="Navn"> Navn </label><br />
-                    <input type="text" id="Navn_" name="Navn" onKeyDown={handleKeyDown}/><br />
-                    <label htmlFor="Sted"> Sted </label><br />
-                    <input type="text" id="Sted_" name="Sted" onKeyDown={handleKeyDown}/>
-                </form>
-                <br /><NextButton/>
-            </div>
+      <div className="FormsGrid">
+        <div className="InputForms">
+          <h1> Skriv deg opp i køen </h1>
+
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="Navn"> Navn </label>
+            <br />
+            <input
+              type="text"
+              id="Navn_"
+              name="Navn"
+              onKeyDown={handleKeyDown}
+            />
+            <br />
+            <label htmlFor="Sted"> Sted </label>
+            <br />
+            <input
+              type="text"
+              id="Sted_"
+              name="Sted"
+              onKeyDown={handleKeyDown}
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+          <br />
         </div>
+      </div>
     </div>
-);
+  );
 }
--- src/navbar_component/App.js
+++ src/navbar_component/App.js
@@ -1,20 +1,22 @@
 import React from "react";
+import "./App.css";
+import profileImg from "./profileImg.png";
+import homeImg from "./homeImg.jpeg";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="Navbar_css">
+      <ul className="Links">
         <li>
-          <a href="#">Home</a>
+          <a href="/">
+            {" "}
+            <img src={homeImg} alt="Home button" className="HomeImg" />
+          </a>
         </li>
         <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
+          <a href="src/login_component/App">
+            <img src={profileImg} alt="Profile button" className="ProfileImg" />
+          </a>
         </li>
       </ul>
     </nav>
